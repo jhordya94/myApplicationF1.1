74da2db5ccef01b510f401e5ce79f24f688b4568
JahirBautistaQ <02210132008@mail.udes.edu.co>
2025-04-09 23:32:00 -0500

    Commit 2: We started using the Material Theme Builder to choose colors, and we also implemented dark mode and light mode.

--- app/src/main/java/com/example/myapplicationf11/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationf11/MainActivity.kt
@@ -11,14 +11,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapplicationf11.ui.theme.MyApplicationF11Theme
+import com.example.myapplicationf11.ui.theme.F1Theme
+//import com.example.woof.ui.theme.WoofTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyApplicationF11Theme {
+            F1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +42,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MyApplicationF11Theme {
+    F1Theme {
         Greeting("Android")
     }
 }
